63cda28e558334aa9e94a6b02f166feb43faadea
Samay Nandan <samaynandan@gmail.com>
2023-05-22 10:13:18 +0530

    Removed Testing Modules

--- backend/src/middleware/routes/routes.middleware.spec.ts
+++
@@ -1,7 +0,0 @@
-import { RoutesMiddleware } from './routes.middleware';
-
-describe('RoutesMiddleware', () => {
-  it('should be defined', () => {
-    expect(new RoutesMiddleware()).toBeDefined();
-  });
-});
