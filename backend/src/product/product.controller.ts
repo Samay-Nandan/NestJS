539a4aa01831dc6b56f51586736fdd005ab189ff
Samay Nandan <samaynandan@gmail.com>
2023-05-30 19:16:50 +0530

    HTTP Status code added in swagger

--- backend/src/product/product.controller.ts
+++ backend/src/product/product.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  HttpStatus,
 } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ProductService } from './product.service';
@@ -19,35 +20,47 @@ export class ProductController {
 
   @Post()
   @ApiOperation({ summary: 'Create a new product' })
-  @ApiResponse({ status: 201, description: 'Returns the Created Product' })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'Returns the Created Product',
+  })
   create(@Body() createProductDto: CreateProductDto) {
     return this.productService.create(createProductDto);
   }
 
   @Get()
   @ApiOperation({ summary: 'Get all products' })
-  @ApiResponse({ status: 200, description: 'Returns all the products' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Returns all the products',
+  })
   findAll() {
     return this.productService.findAll();
   }
 
   @Get(':id')
   @ApiOperation({ summary: 'Get product by id' })
-  @ApiResponse({ status: 200, description: 'Returns product by id' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Returns product by id' })
   findOne(@Param('id') id: string) {
     return this.productService.findOne(id);
   }
 
   @Patch(':id')
   @ApiOperation({ summary: 'Update product by id' })
-  @ApiResponse({ status: 200, description: 'Returns updated product by id' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Returns updated product by id',
+  })
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productService.update(id, updateProductDto);
   }
 
   @Delete(':id')
   @ApiOperation({ summary: 'Delete product by id' })
-  @ApiResponse({ status: 200, description: 'Returns deleted product id only' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Returns deleted product id only',
+  })
   remove(@Param('id') id: string) {
     return this.productService.remove(id);
   }
